509e778bc04554f0c822d774b7d942402da92b81
kerbychuable <kerbychua@gmail.com>
2021-12-31 21:40:23 +0800

    fix normalizer.py to support txt files

--- normalizer.py
+++ normalizer.py
@@ -8,18 +8,16 @@ normalizedFilename = './normalized.csv'
 ext = os.path.splitext(filename)[-1].lower()
 try:
     if 'xls' in ext:
-
-
         fileReader = pd.read_excel (filename, engine = 'openpyxl')
         fileReader.to_csv(normalizedFilename, index = None, header = True)
-    elif 'csv' in ext:
+    elif 'csv' in ext or 'txt' in ext:
         try:
             with open(filename, newline='') as fileIn, open(normalizedFilename, 'w') as fileOut:
                 for line in fileIn:
                     line = line.replace('\r','')
-                    line.replace(';', ',')
-                    line.replace('\t', ',')
-                    line.replace('|', ',')
+                    line = line.replace(';', ',')
+                    line = line.replace('\t', ',')
+                    line = line.replace('|', ',')
                     fileOut.write(line)
         except Exception as e:
             print(e)
