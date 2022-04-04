c44e6475bb9e18afae188ca772a5ca3f08b7d14f
kerbychuable <kerbychua@gmail.com>
2022-04-04 14:21:20 +0800

    clean up

--- decompressor/decompressor.py
+++ decompressor/decompressor.py
@@ -4,10 +4,7 @@ from os.path import exists
 
 class Decompressor:
     def log(self, filename, decompression_time, original_size, compressed_size, compression_ratio):
-        # index = len(self.history)
         log_time = datetime.now()
-        # log_message = "[%d] %s - Decompressed %s using %s. Time elapsed: %ss. Original size: %s B. Compressed size: %s B. Compression ratio: %.2f%%\n" % (index, log_time, filename, self.name, compression_time, original_size, compressed_size, compression_ratio)
-        # self.history.append(log_message)
 
         date = log_time.strftime("%Y-%m-%d")
         log_filename = 'decomp %s.csv' % (date)
