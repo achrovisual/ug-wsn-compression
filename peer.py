b0d074900b77c8dc9860a9372de0d26e926e792d
Eugenio Pastoral <gino@achrovisual.com>
2022-04-30 11:04:22 +0800

    Fixed construction of received data

--- peer.py
+++ peer.py
@@ -10,6 +10,7 @@ SEPARATOR = "<SEPARATOR>"
 
 xbee = serial.Serial('/dev/ttyUSB0', 9600)
 print(f'[+] Connected to Xbee.')
+count = 0
 
 while True:
     while xbee.in_waiting < 0:
@@ -17,28 +18,43 @@ while True:
     received = xbee.readline().decode()[:-1]
     print(received)
     algorithm, filesize, md5 = received.split(SEPARATOR)
-    filesize = int(filesize)
+    filesize = int(filesize) + 16
     data = b''
     decomp_data = None
+
+    # print(filesize)
 
     progress = tqdm.tqdm(range(filesize), f"Receiving sensor readings", unit="B", unit_scale=True, unit_divisor=1024)
 
     while True:
-        if xbee.in_waiting > 0:
-            bytes_read = xbee.read()
-            filesize = filesize - len(bytes_read)
-            data += bytes_read
-            progress.update(len(bytes_read))
-        if filesize <= 0:
+        # print(sys.getsizeof(data))
+        # print(xbee.in_waiting)
+        # print(sys.getsizeof(data))
+        # if xbee.in_waiting > 0:
+        bytes_read = xbee.read()
+        # filesize = filesize - sys.getsizeof(bytes_read)
+        data += bytes_read
+        progress.update(sys.getsizeof(bytes_read))
+
+        if filesize == sys.getsizeof(data):
+            # print(data)
+            if algorithm != 'lzw':
+                count = 0
+
             if algorithm == 'lzma':
                 lzma_comp = LZMA()
                 decomp_data = lzma_comp.decompress(None, data)
             elif algorithm == 'lzw':
-                with open("temp.Z", "wb") as file_in:
+                temp = "normalized.csv" + '_' + str(count)
+
+                with open(temp+'.Z', "wb") as file_in:
                     file_in.write(data)
+                    file_in.close()
+                count = count + 1
                 lzw_comp = LZW()
-                lzw_comp.decompress(temp)
-                with open('temp', 'rb') as f:
+                lzw_comp.decompress(temp+'.Z', temp)
+
+                with open(temp, 'rb') as f:
                     decomp_data = f.read()
             elif algorithm == 'bzip2':
                 bz_comp = bzip2()
@@ -52,7 +68,7 @@ while True:
 
             md5_decomp = integrity(None, decomp_data)
             if md5 == md5_decomp:
-                print('MD5 verified.')
+                print('\nMD5 verified.')
             else:
-                print('MD5 verification failed.')
+                print('\nMD5 verification failed.')
             break
